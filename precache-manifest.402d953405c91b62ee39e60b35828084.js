92584c91b4d28c818cd2a1dd6377d8e615919ab3
goulmaster <botskiiakk@gmail.com>
2022-05-14 11:53:36 +0600

    Updates

---
+++ precache-manifest.402d953405c91b62ee39e60b35828084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a187502fd8f1dfe06c55",
+    "url": "/12/static/css/main.cafa9357.chunk.css"
+  },
+  {
+    "revision": "a187502fd8f1dfe06c55",
+    "url": "/12/static/js/main.a187502f.chunk.js"
+  },
+  {
+    "revision": "11d5520fcb1746ecea44",
+    "url": "/12/static/js/1.11d5520f.chunk.js"
+  },
+  {
+    "revision": "ebf0a93219bddc5fc28f",
+    "url": "/12/static/js/runtime~main.ebf0a932.js"
+  },
+  {
+    "revision": "d7024eef027be954b9875c7b05d43124",
+    "url": "/12/index.html"
+  }
+];
